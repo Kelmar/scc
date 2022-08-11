c07b09bd700720f9ac7efbaa26ae10397cdfc441
Kelmar Firesun <kfiresun@gmail.com>
2022-08-11 14:42:59 -0500

    Minor doc update

--- scc1/src/main.c
+++ scc1/src/main.c
@@ -7,17 +7,6 @@
 #include "lexer.h"
 #include "parser.h"
 
-/* ===================================================================== */
-
-/*
- * Stage 1 compiling.
- * 
- * This occurs after the preprocessor has done it's thing.
- * 
- * We need to keep an eye out for "#line" directives while lexing; that will
- * change the file name and line number, which we'll need for error reporting.
- */
-
  /* ===================================================================== */
 
 char g_filename[1024] = { 0 };
